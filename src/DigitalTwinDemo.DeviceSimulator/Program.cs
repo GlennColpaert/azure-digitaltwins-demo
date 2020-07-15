624eac9f3300e285a7cb4ad73112c0d6832e014b
GlennColpaert <glenn_colpaert@hotmail.com>
2020-07-15 14:30:31 +0200

    Update DeviceSimulator to transmit data to IoTHub

--- src/DigitalTwinDemo.DeviceSimulator/Program.cs
+++ src/DigitalTwinDemo.DeviceSimulator/Program.cs
@@ -1,23 +1,47 @@
-﻿using System;
+﻿using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System;
+using System.Configuration;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace DigitalTwinDemo.DeviceSimulator
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Task task1 = Task.Factory.StartNew(() => doStuff());
-            Task task2 = Task.Factory.StartNew(() => doStuff());
-            Task task3 = Task.Factory.StartNew(() => doStuff());
+            string sDevice01 = ConfigurationManager.AppSettings["DEVICE001"];
+            string sDevice02 = ConfigurationManager.AppSettings["DEVICE002"];
 
-            Task.WaitAll(task1, task2, task3);
-            Console.WriteLine("All threads complete");
+            Task task1 = Task.Factory.StartNew(() => doStuffAsync("Device001",sDevice01));
+            Task task2 = Task.Factory.StartNew(() => doStuffAsync("Device002", sDevice02));
+
+            await Task.WhenAll(task1, task2);
+            Console.ReadLine();
         }
 
-        static void doStuff()
+        static async Task doStuffAsync(string deviceName, string connectionString)
         {
-            //do stuff here
+            var deviceClient = DeviceClient.CreateFromConnectionString(connectionString);
+
+            double avgTemperature = 21;
+            Random rand = new Random();
+
+            while (true)
+            {
+                double currentTemperature = avgTemperature + rand.NextDouble() * 4 - 3;
+
+                var telemetryDataPoint = new
+                {
+                    Temperature = currentTemperature
+                };
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+                await deviceClient.SendEventAsync(message);
+                Console.WriteLine("{0} > Sending message for Device {1}: {2}", DateTime.Now, deviceName, messageString);
+                await Task.Delay(5000);
+            }
         }
     }
 }
